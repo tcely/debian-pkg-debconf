ebf7435384c851214cff7fd9c30d53b775e34a6d
Colin Watson <cjwatson@debian.org>
2017-07-03 06:03:39 +0100

    Change directory to / before executing maintainer scripts (closes: #608648).

--- Debconf/ConfModule.pm
+++ Debconf/ConfModule.pm
@@ -147,10 +147,16 @@ sub startup {
 	push @args, @_ if @_;
 	
 	debug developer => "starting ".join(' ',@args);
-	$this->pid(open2($this->read_handle(FileHandle->new),
-		         $this->write_handle(FileHandle->new),
-			 @args)) || die $!;
-		
+	my $pid = open2($this->read_handle(FileHandle->new),
+		        $this->write_handle(FileHandle->new),
+		        '-');
+	die "can't fork: $!\n" unless defined $pid;
+	unless ($pid) {
+		chdir('/');
+		exec(@args);
+	}
+	$this->pid($pid);
+
 	# Catch sigpipes so they don't kill us, and return 128 for them.
 	$this->caught_sigpipe('');
 	$SIG{PIPE}=sub { $this->caught_sigpipe(128) };
